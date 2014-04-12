e0e0885057f05368569c9e9e38cc437107833275
Chris Thomas <webmaster102@gmail.com>
2014-04-11 23:32:11 -0400

    Delete email.js

--- email.js
+++
@@ -1,8 +0,0 @@
-function sendFormByEmail(e) {
-  var email = "chriszine102@yahoo.com";
-  var txt = "";
-  for (var field in e.namedValues) {
-    txt += field + ' :: ' + e.namedValues[field].toString() + "\n\n";
-  }
-  MailApp.sendEmail(email, "Website Form Submitted", txt);
-}
